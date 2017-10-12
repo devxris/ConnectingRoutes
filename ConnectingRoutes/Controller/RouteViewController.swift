6e157c6a4d726a8752b93051029b58f3774721a3
devxris <chuwei0915@gmail.com>
2017-10-12 16:32:28 +0800

    animate MKPointAnnotation by mapView delegate method

--- ConnectingRoutes/Controller/RouteViewController.swift
+++ ConnectingRoutes/Controller/RouteViewController.swift
@@ -15,6 +15,7 @@ class RouteViewController: UIViewController {
 
 	@IBOutlet weak var mapView: MKMapView! {
 		didSet {
+			mapView.delegate = self
 			let longPress = UILongPressGestureRecognizer(target: self, action: #selector(pinLocation(recongnizer:)))
 			longPress.minimumPressDuration = 0.3
 			mapView.addGestureRecognizer(longPress)
@@ -39,5 +40,17 @@ class RouteViewController: UIViewController {
 		annotations.append(annotation)
 		
 		mapView.showAnnotations([annotation], animated: true)
+	}
+}
+
+extension RouteViewController: MKMapViewDelegate {
+	
+	func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+		let annotationView = views[0]
+		let endFrame = annotationView.frame
+		annotationView.frame = endFrame.offsetBy(dx: 0, dy: -600)
+		UIView.animate(withDuration: 0.4) {
+			annotationView.frame = endFrame
+		}
 	}
 }
