ad88d49b727a8ea465902f871f1f08bd23550a23
devxris <chuwei0915@gmail.com>
2017-10-12 16:14:53 +0800

    Simple UI and setup

---
+++ ConnectingRoutes/Controller/RouteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RouteViewController.swift
+//  ConnectingRoutes
+//
+//  Created by Chris Huang on 12/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RouteViewController: UIViewController {
+
+	@IBOutlet weak var mapView: MKMapView!
+}
+
