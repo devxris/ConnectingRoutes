2b26588ed40bf36c5a94bee71eec8d86b987ed37
devxris <chuwei0915@gmail.com>
2017-10-12 16:29:24 +0800

    add MKPointAnnotation after long press

--- ConnectingRoutes/Controller/RouteViewController.swift
+++ ConnectingRoutes/Controller/RouteViewController.swift
@@ -10,7 +10,34 @@ import UIKit
 import MapKit
 
 class RouteViewController: UIViewController {
+	
+	private var annotations = [MKPointAnnotation]()
 
-	@IBOutlet weak var mapView: MKMapView!
+	@IBOutlet weak var mapView: MKMapView! {
+		didSet {
+			let longPress = UILongPressGestureRecognizer(target: self, action: #selector(pinLocation(recongnizer:)))
+			longPress.minimumPressDuration = 0.3
+			mapView.addGestureRecognizer(longPress)
+		}
+	}
+	
+	@objc func pinLocation(recongnizer: UILongPressGestureRecognizer) {
+		
+		if recongnizer.state != .ended { return }
+		
+		// Get the location of the press
+		let tappedPoint = recongnizer.location(in: mapView)
+		
+		// Convert the location from a point to a coordinate
+		let tappedCoordinate = mapView.convert(tappedPoint, toCoordinateFrom: mapView)
+		
+		// With the coordinate, annotate the location on the map
+		let annotation = MKPointAnnotation()
+		annotation.coordinate = tappedCoordinate
+		
+		// Store the annotation for later use
+		annotations.append(annotation)
+		
+		mapView.showAnnotations([annotation], animated: true)
+	}
 }
-
