b003c957087117c9e8ffbddf38c8431775a24e26
devxris <chuwei0915@gmail.com>
2017-10-12 16:48:35 +0800

    Draw polyline in renderer by mapView delegate method

--- ConnectingRoutes/Controller/RouteViewController.swift
+++ ConnectingRoutes/Controller/RouteViewController.swift
@@ -41,10 +41,23 @@ class RouteViewController: UIViewController {
 		
 		mapView.showAnnotations([annotation], animated: true)
 	}
+	
+	@IBAction func drawPolyLine(_ sender: UIBarButtonItem) {
+		
+		// remove current overlays
+		mapView.removeOverlays(mapView.overlays)
+		
+		// add MKPolyLine(conform to MKOverlay) to mapView
+		var coordinates = [CLLocationCoordinate2D]()
+		annotations.forEach { coordinates.append($0.coordinate) }
+		let polyline = MKPolyline(coordinates: &coordinates, count: coordinates.count)
+		mapView.add(polyline)
+	}
 }
 
 extension RouteViewController: MKMapViewDelegate {
 	
+	// animate pin drop
 	func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
 		let annotationView = views[0]
 		let endFrame = annotationView.frame
@@ -52,5 +65,20 @@ extension RouteViewController: MKMapViewDelegate {
 		UIView.animate(withDuration: 0.4) {
 			annotationView.frame = endFrame
 		}
+	}
+	
+	// draw render overlay
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		
+		let renderer = MKPolylineRenderer(overlay: overlay)
+		renderer.lineWidth = 3.0
+		renderer.strokeColor = .purple
+		renderer.alpha = 0.5
+		
+		// relocate mapView to visible mapRect
+		let visibleRect = mapView.mapRectThatFits(renderer.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50))
+		mapView.setRegion(MKCoordinateRegionForMapRect(visibleRect), animated: true)
+		
+		return renderer
 	}
 }
